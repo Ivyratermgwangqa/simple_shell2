716c10c471b6e9a6e92dcecd7ed68a40e71a740c
Ivyratermgwangqa <ivyratermgwangqa@gmail.com>
2023-08-07 12:03:56 -0700

    creating entry point and header file

--- shell.c
+++ shell.c
@@ -5,17 +5,19 @@
  *
  *
  */
-int main()
+int main(void)
 {
 	char *prompt = "myshell> ";
+
 	while (1)
 	{
 		printf("%s", prompt);
 		char *command = read_command();
 		char **argv = parse_command(command);
+
 		execute_command(argv);
 		free(command);
 		free(argv);
 	}
-	return 0;
+	return (0);
 }
